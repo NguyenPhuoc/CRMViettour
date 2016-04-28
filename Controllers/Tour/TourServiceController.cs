9d8b4bcefc5bb77d843f684f902277efd9f80aa7
Võ Ái Duy <voaiduy261291@gmail.com>
2016-04-28 08:20:06 +0700

    tour_vs5

--- Controllers/Tour/TourServiceController.cs
+++ Controllers/Tour/TourServiceController.cs
@@ -1,8 +1,11 @@
 ﻿using CRM.Core;
 using CRM.Infrastructure;
+using CRMViettour.Models;
+using CRMViettour.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -13,6 +16,7 @@ namespace CRMViettour.Controllers.Tour
         // GET: TourService
         #region Init
 
+        private IGenericRepository<tbl_Company> _companyRepository;
         private IGenericRepository<tbl_Dictionary> _dictionaryRepository;
         private IGenericRepository<tbl_ServicesPartner> _servicesPartnerRepository;
         private IGenericRepository<tbl_Tour> _tourRepository;
@@ -36,6 +40,7 @@ namespace CRMViettour.Controllers.Tour
         private DataContext _db;
 
         public TourServiceController(IGenericRepository<tbl_Dictionary> dictionaryRepository,
+            IGenericRepository<tbl_Company> companyRepository,
             IGenericRepository<tbl_ServicesPartner> servicesPartnerRepository,
             IGenericRepository<tbl_Tour> tourRepository,
             IGenericRepository<tbl_ReviewTour> reviewTourRepository,
@@ -59,6 +64,7 @@ namespace CRMViettour.Controllers.Tour
             : base(baseRepository)
         {
             this._dictionaryRepository = dictionaryRepository;
+            this._companyRepository = companyRepository;
             this._servicesPartnerRepository = servicesPartnerRepository;
             this._tourRepository = tourRepository;
             this._reviewTourRepository = reviewTourRepository;
@@ -99,13 +105,130 @@ namespace CRMViettour.Controllers.Tour
 
         #region Sự kiện
 
+        /// <summary>
+        /// upload file sự kiện
+        /// </summary>
+        /// <param name="FileName"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult UploadFileEvent(HttpPostedFileBase FileName, int id)
+        {
+            Session["EventFile" + id] = FileName;
+            return Json(JsonRequestBehavior.AllowGet);
+        }
 
+        /// <summary>
+        /// thêm mới sự kiện
+        /// </summary>
+        /// <param name="form"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [ValidateInput(false)]
+        public async Task<ActionResult> CreateEvent(FormCollection form)
+        {
+            try
+            {
+                int tourId = Convert.ToInt32(Session["idTour"].ToString());
+                for (int i = 1; i <= Convert.ToInt32(form["NumberOptionEventService"].ToString()); i++)
+                {
+                    //file
+                    HttpPostedFileBase FileName = Session["EventFile" + i] as HttpPostedFileBase;
+                    String newName = "";
+                    if (FileName != null && FileName.ContentLength > 0)
+                    {
+                        string FileSize = Common.ConvertFileSize(FileName.ContentLength);
+                        newName = FileName.FileName.Insert(FileName.FileName.LastIndexOf('.'), String.Format("{0:_ddMMyyyy}", DateTime.Now));
+                        String path = Server.MapPath("~/Upload/file/" + newName);
+                        FileName.SaveAs(path);
+                    }
+                    //end file
 
+                    var model = new tbl_Partner()
+                    {
+                        CompanyId = Convert.ToInt32(form["CompanyId" + i].ToString()),
+                        Name = _companyRepository.FindId(Convert.ToInt32(form["CompanyId" + i].ToString())).Name,
+                        Code = form["Code" + i].ToString(),
+                        FileName = newName,
+                        Price = Convert.ToDecimal(form["Price" + i].ToString()),
+                        StaffContact = form["StaffContact" + i].ToString(),
+                        Phone = form["Phone" + i].ToString(),
+                        CreatedDate = DateTime.Now,
+                        ModifiedDate = DateTime.Now,
+                        Note = form["Note" + i].ToString(),
+                        IsUse = true,
+                        DictionaryId = 1051
+                    };
+
+                    if (await _partnerRepository.Create(model))
+                    {
+                        var item = new tbl_TourOption
+                        {
+                            PartnerId = model.Id,
+                            ServiceId = 1051,
+                            TourId = tourId
+                        };
+                        await _tourOptionRepository.Create(item);
+                    }
+                }
+                var list = _tourOptionRepository.GetAllAsQueryable().AsEnumerable().Where(p => p.TourId == tourId).
+                             Select(p => new TourServiceViewModel
+                             {
+                                 Id = p.Id,
+                                 Code = p.tbl_Partner.Code,
+                                 ServiceId = _dictionaryRepository.FindId(p.tbl_Partner.DictionaryId).Id,
+                                 ServiceName = _dictionaryRepository.FindId(p.tbl_Partner.DictionaryId).Name,
+                                 Name = p.tbl_Partner.Name,
+                                 Address = p.tbl_Partner.Address,
+                                 StaffContact = p.tbl_Partner.StaffContact,
+                                 Phone = p.tbl_Partner.Phone,
+                                 Price = p.tbl_Partner.Price,
+                                 Note = p.tbl_Partner.Note,
+                                 TourOptionId = p.Id,
+                                 TourId = p.TourId
+                             }).ToList();
+                return PartialView("_Partial_InsertEvent", list);
+            }
+            catch { }
+
+            return PartialView("_Partial_InsertEvent");
+        }
         #endregion
 
 
         #region Khác
         #endregion
 
+        #region Xóa dịch vụ
+
+        [HttpPost]
+        public async Task<ActionResult> DeleteService(int tourid, int optionid)
+        {
+            try
+            {
+                await _tourOptionRepository.Delete(optionid, true);
+                var list = _tourOptionRepository.GetAllAsQueryable().AsEnumerable().Where(p => p.TourId == tourid).
+                            Select(p => new TourServiceViewModel
+                            {
+                                Id = p.Id,
+                                Code = p.tbl_Partner.Code,
+                                ServiceId = _dictionaryRepository.FindId(p.tbl_Partner.DictionaryId).Id,
+                                ServiceName = _dictionaryRepository.FindId(p.tbl_Partner.DictionaryId).Name,
+                                Name = p.tbl_Partner.Name,
+                                Address = p.tbl_Partner.Address,
+                                StaffContact = p.tbl_Partner.StaffContact,
+                                Phone = p.tbl_Partner.Phone,
+                                Price = p.tbl_Partner.Price,
+                                Note = p.tbl_Partner.Note,
+                                TourOptionId = p.Id,
+                                TourId = p.TourId
+                            }).ToList();
+                return PartialView("_Partial_InsertEvent", list);
+            }
+            catch
+            {
+                return PartialView("_Partial_InsertEvent");
+            }
+        }
+        #endregion
     }
 }
---
+++ Scripts/controller/tourstatus.js
@@ -0,0 +1,56 @@
+﻿
+$("#filter-tour").select2();
+
+$('.dataTable').dataTable({
+    order: [],
+    columnDefs: [{ orderable: false, targets: [0] }]
+});
+
+$(".dataTable").dataTable().columnFilter({
+    sPlaceHolder: "head:after",
+    aoColumns: [null,
+                { type: "text" },
+                { type: "text" },
+                { type: "text" },
+                { type: "text" },
+                { type: "text" },
+                { type: "text" },
+                { type: "text" },
+                { type: "text" },
+                { type: "text" },
+                { type: "text" }]
+});
+
+$("#filter-tour").change(function () {
+    var dataPost = { key: $("#filter-tour").val() };
+    $.ajax({
+        type: "POST",
+        url: '/TourStatus/FilterTour',
+        data: JSON.stringify(dataPost),
+        contentType: "application/json; charset=utf-8",
+        dataType: "html",
+        success: function (data) {
+            $("#data-tour").html(data);
+            /****************/
+            $('.dataTable').dataTable({
+                order: [],
+                columnDefs: [{ orderable: false, targets: [0] }]
+            });
+
+            $(".dataTable").dataTable().columnFilter({
+                sPlaceHolder: "head:after",
+                aoColumns: [null,
+                            { type: "text" },
+                            { type: "text" },
+                            { type: "text" },
+                            { type: "text" },
+                            { type: "text" },
+                            { type: "text" },
+                            { type: "text" },
+                            { type: "text" },
+                            { type: "text" },
+                            { type: "text" }]
+            });
+        }
+    });
+})

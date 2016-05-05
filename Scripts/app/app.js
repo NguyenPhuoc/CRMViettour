31e2d9cf0dc9d57e4ae66e703f4800ea1f9fb44d
Võ Ái Duy <voaiduy261291@gmail.com>
2016-05-05 17:27:08 +0700

    remove icon calendar
    remove icon calendar

--- Scripts/app/app.js
+++ Scripts/app/app.js
@@ -24,3 +24,31 @@ app.controller('subitemCtrl', function ($scope) {
         $scope.subitems.splice(lastItem);
     };
 });
+
+
+/************ HOTEL ************/
+
+app.controller('itemCtrlHotel', function ($scope) {
+    $scope.items = [1];
+    $scope.addNewItemHotel = function () {
+        var newItemNo = $scope.items.length + 1;
+        $scope.items.push(newItemNo);
+    };
+    $scope.removeItemHotel = function () {
+        var lastItem = $scope.items.length - 1;
+        $scope.items.splice(lastItem);
+    };
+
+});
+
+app.controller('subitemCtrlHotel', function ($scope) {
+    $scope.subitems = [1];
+    $scope.addNewItemHotel1 = function () {
+        var newItemNo = $scope.subitems.length + 1;
+        $scope.subitems.push(newItemNo);
+    };
+    $scope.removeItemHotel1 = function () {
+        var lastItem = $scope.subitems.length - 1;
+        $scope.subitems.splice(lastItem);
+    };
+});
